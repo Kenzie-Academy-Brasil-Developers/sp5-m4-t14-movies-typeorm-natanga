fc38ee436dc88ea4f066679df6df2fad2d86a8f2
natabgaf <natanfernandes52@gmail.com>
2023-02-28 10:24:31 -0300

    feat/delete-updatwe

--- src/services/createMovies.service.ts
+++ src/services/createMovies.service.ts
@@ -1,4 +1,18 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../data-source';
+import { Movie } from '../entities';
+import { iMoviesRequest, iMoviesResponse } from "../interfaces/movies.Interface";
+import { responseMovieSchema } from '../schemas/movies.schema';
 
-export const createMoviesService =async (params:any) => {
-    
+export const createMoviesService = async (dataMovies: iMoviesRequest): Promise<iMoviesResponse> => {
+
+    const movieRepository: Repository<Movie> = AppDataSource.getRepository(Movie);
+
+    const movie = movieRepository.create(dataMovies);
+
+    await movieRepository.save(movie);
+
+    const newMovie = responseMovieSchema.parse(movie);
+
+    return newMovie
 }
